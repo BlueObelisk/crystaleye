5104d4b311a3b7ef82ece1e07acb640fe9ee7357
ned24 <ned24@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2008-10-20 17:00:12 +0000

    More refactoring work. STILL BROKEN, DO NOT USE.

--- src/main/java/wwmm/crystaleye/crawlers/ArticleDetails.java
+++ src/main/java/wwmm/crystaleye/crawlers/ArticleDetails.java
@@ -1,5 +1,67 @@
 package wwmm.crystaleye.crawlers;
 
+import java.util.List;
+
+import org.apache.commons.httpclient.URI;
+
 public class ArticleDetails {
+	
+	private URI doi;
+	private String title;
+	private String reference;
+	private String authors;
+	private List<SupplementaryFile> suppFiles;
+	
+	public ArticleDetails() {
+		;
+	}
+	
+	public ArticleDetails(URI doi, String title, String ref, String authors, List<SupplementaryFile> suppFiles) {
+		this.doi = doi;
+		this.title = title;
+		this.reference = ref;
+		this.authors = authors;
+		this.suppFiles = suppFiles;
+	}
+
+	public List<SupplementaryFile> getSuppFiles() {
+		return suppFiles;
+	}
+
+	public void setSuppFiles(List<SupplementaryFile> suppFiles) {
+		this.suppFiles = suppFiles;
+	}
+
+	public void setDoi(URI doi) {
+		this.doi = doi;
+	}
+
+	public URI getDoi() {
+		return doi;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public String getAuthors() {
+		return authors;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public void setAuthors(String authors) {
+		this.authors = authors;
+	}
 
 }
