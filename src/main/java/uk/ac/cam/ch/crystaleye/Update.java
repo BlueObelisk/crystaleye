db7ba5bd2576c37419434f4d05f5d9895520b01c
ned24 <ned24@cam.ac.uk>
2009-03-02 12:25:10 +0000

    Rewrite Update so that various command line options can be passed in to run only one manager at a time and not just the whole lot.

--- src/main/java/uk/ac/cam/ch/crystaleye/Update.java
+++ src/main/java/uk/ac/cam/ch/crystaleye/Update.java
@@ -25,8 +25,6 @@ import uk.ac.cam.ch.crystaleye.site.feeds.RssManager;
 
 public class Update implements CMLConstants {
 
-	private SiteProperties properties;
-
 	public static void main(String[] args) {
 		Update launcher = new Update();
 		launcher.run(args);
@@ -34,58 +32,104 @@ public class Update implements CMLConstants {
 
 	public void run(String[] args) {
 		Options options = new Options();
-		Option option = null;
-
-		option = OptionBuilder.withArgName("PROPERTIES PATH").hasArg().withDescription("Path to properties file").create("p");
-		options.addOption(option);
+		options.addOption("p", true, "Path to properties file");
+		options.addOption("all", false, "Run all manager");
+		options.addOption("fetch", false, "Run fetch manager");
+		options.addOption("cif2cml", false, "Run cif2cml manager");
+		options.addOption("cml2foo", false, "Run cml2foo manager");
+		options.addOption("webpage", false, "Run webpage manager");
+		options.addOption("doilist", false, "Run doilist manager");
+		options.addOption("cellparams", false, "Run cellparams manager");
+		options.addOption("bondlengths", false, "Run bondlengths manager");
+		options.addOption("smiles", false, "Run smiles manager");
+		options.addOption("atompub", false, "Run atompub manager");
+		options.addOption("rss", false, "Run rss manager");
 
 		CommandLineParser parser = new BasicParser();
-		CommandLine commandLine = null;
-
+		CommandLine cl = null;
 		try {
-			commandLine = parser.parse(options, args);
-		}
-		catch(ParseException e) {
-			printUsage();
+			cl = parser.parse(options, args);
+		} catch(ParseException e) {
+			e.printStackTrace();
 			return;
 		}
 
-		String propsPath = commandLine.getOptionValue("p");
-		properties = new SiteProperties(new File(propsPath));
-
-		if (propsPath != null) {
-			FetchManager fetch = new FetchManager(propsPath);
-			fetch.run();
-			Cif2CmlManager cif2Cml = new Cif2CmlManager(propsPath);
-			cif2Cml.execute();
-			CML2FooManager cml2Foo = new CML2FooManager(propsPath);
-			cml2Foo.execute();
-			WebpageManager webpage = new WebpageManager(propsPath);
-			webpage.execute();
-			DoiListManager dois = new DoiListManager(propsPath);
-			dois.execute();
-			CellParamsManager cell = new CellParamsManager(propsPath);
-			cell.execute();
-			BondLengthsManager bond = new BondLengthsManager(propsPath);
-			bond.execute();
-			SmilesListManager smi = new SmilesListManager(propsPath);
-			smi.execute();
-			AtomPubManager ap = new AtomPubManager(propsPath);
-			ap.execute();
-			// before updating the RSS feeds, go through them all and delete any entries over x days old
-			// new RSSArchiver().deleteOldFeedEntries("xml", properties.getRssWriteDir(), 14);
-			RssManager rss = new RssManager(propsPath);
-			rss.execute();
+		String propsPath = cl.getOptionValue("p");
+		if (propsPath == null) {
+			throw new RuntimeException("No properties path set.");
+		}
+		if (args.length == 2) {
+			runall(propsPath);
 		} else {
-			printUsage();
+			if (cl.hasOption("all")) {
+				runall(propsPath);
+			}
+			if (cl.hasOption("fetch")) {
+				FetchManager fetch = new FetchManager(propsPath);
+				fetch.run();
+			}
+			if (cl.hasOption("cif2cml")) {
+				Cif2CmlManager cif2Cml = new Cif2CmlManager(propsPath);
+				cif2Cml.execute();
+			}
+			if (cl.hasOption("cml2foo")) {
+				CML2FooManager cml2Foo = new CML2FooManager(propsPath);
+				cml2Foo.execute();
+			}
+			if (cl.hasOption("webpage")) {
+				WebpageManager webpage = new WebpageManager(propsPath);
+				webpage.execute();
+			}
+			if (cl.hasOption("doilist")) {
+				DoiListManager dois = new DoiListManager(propsPath);
+				dois.execute();
+			}
+			if (cl.hasOption("cellparams")) {
+				CellParamsManager cell = new CellParamsManager(propsPath);
+				cell.execute();
+			}
+			if (cl.hasOption("bondlengths")) {
+				BondLengthsManager bond = new BondLengthsManager(propsPath);
+				bond.execute();
+			}
+			if (cl.hasOption("smiles")) {
+				SmilesListManager smi = new SmilesListManager(propsPath);
+				smi.execute();
+			}
+			if (cl.hasOption("atompub")) {
+				AtomPubManager ap = new AtomPubManager(propsPath);
+				ap.execute();
+			}
+			if (cl.hasOption("rss")) {
+				RssManager rss = new RssManager(propsPath);
+				rss.execute();
+			}
 		}
 	}
-
-	private void printUsage() {
-		System.out.println("Usage: cifmanager -p [PATH]");
-		System.out.println("Execute CIF manager");
-		System.out.println();
-		System.out.println("  -p, --path to properties file");
-		System.out.println();
+	
+	private void runall(String propsPath) {
+		FetchManager fetch = new FetchManager(propsPath);
+		fetch.run();
+		Cif2CmlManager cif2Cml = new Cif2CmlManager(propsPath);
+		cif2Cml.execute();
+		CML2FooManager cml2Foo = new CML2FooManager(propsPath);
+		cml2Foo.execute();
+		WebpageManager webpage = new WebpageManager(propsPath);
+		webpage.execute();
+		DoiListManager dois = new DoiListManager(propsPath);
+		dois.execute();
+		CellParamsManager cell = new CellParamsManager(propsPath);
+		cell.execute();
+		BondLengthsManager bond = new BondLengthsManager(propsPath);
+		bond.execute();
+		SmilesListManager smi = new SmilesListManager(propsPath);
+		smi.execute();
+		AtomPubManager ap = new AtomPubManager(propsPath);
+		ap.execute();
+		// before updating the RSS feeds, go through them all and delete any entries over x days old
+		// new RSSArchiver().deleteOldFeedEntries("xml", properties.getRssWriteDir(), 14);
+		RssManager rss = new RssManager(propsPath);
+		rss.execute();
 	}
+
 }
