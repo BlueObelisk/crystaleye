1c4551522bd4be123ad3ab130951f16d3b31f5e4
ned24 <ned24@cam.ac.uk>
2009-03-11 22:04:12 +0000

    Added more comments.

--- src/main/java/wwmm/crystaleye/index/DoiVsCifFilenameIndex.java
+++ src/main/java/wwmm/crystaleye/index/DoiVsCifFilenameIndex.java
@@ -6,6 +6,16 @@ import org.apache.log4j.Logger;
 
 import wwmm.crystaleye.crawler.DOI;
 
+/**
+ * <p>
+ * Provides methods for access and manipulation for an index 
+ * of a CIF file's DOI of origin against it's filename on the
+ * site it was found.
+ * </p>
+ * 
+ * @author Nick Day
+ * @version 0.1
+ */
 public class DoiVsCifFilenameIndex extends StringAsKeyIndex {
 
 	public static final String INDEX_FILENAME = "doi-ciffilename_index.txt";
@@ -16,14 +26,47 @@ public class DoiVsCifFilenameIndex extends StringAsKeyIndex {
 		super(storageRoot, INDEX_FILENAME);
 	}
 	
+	/**
+	 * <p>
+	 * Insert an entry into the index with the provided DOI and
+	 * filename.
+	 * </p>
+	 * 
+	 * @param doi of the entry to be added.
+	 * @param filename of the entry to be added.
+	 * 
+	 * @return true if the entry was successfully added to the 
+	 * index, false if not.
+	 */
 	public boolean insert(DOI doi, String filename) {
 		return super.insert(doi.getPostfix(), filename);
 	}
 	
+	/**
+	 * 
+	 * 
+	 * @param doi
+	 * @param filename
+	 * @param dontDuplicate
+	 * 
+	 * @return
+	 */
 	public boolean insert(DOI doi, String filename, boolean dontDuplicate) {
 		return super.insert(doi.getPostfix(), filename, dontDuplicate);
 	}
 	
+	/**
+	 * <p>
+	 * Finds whether an entry already exists in the index with the
+	 * provided DOI and filename.
+	 * </p>
+	 * 
+	 * @param doi of the entry to be searched for.
+	 * @param filename of the entry to be searched for.
+	 * 
+	 * @return true if the index already contains an entry with the
+	 * provided DOI and filename, false if not.
+	 */
 	public boolean contains(DOI doi, String filename) {
 		return super.contains(doi.getPostfix(), filename);
 	}
--- src/main/java/wwmm/crystaleye/task/CrawlerTask.java
+++ src/main/java/wwmm/crystaleye/task/CrawlerTask.java
@@ -19,6 +19,15 @@ import wwmm.crystaleye.index.PrimaryKeyVsDoiIndex;
 import wwmm.crystaleye.model.ArticleMetadataDAO;
 import wwmm.crystaleye.model.CifFileDAO;
 
+/**
+ * <p>
+ * Main class for handling the scraping of CIFs from websites and
+ * writing them to the CrystalEye database.
+ * </p>
+ * 
+ * @author Nick Day
+ * @version 0.1
+ */
 public class CrawlerTask {
 	
 	// TODO pull out all the DAO stuff and moved to a CIFRecordDAO.
@@ -36,6 +45,14 @@ public class CrawlerTask {
 		this.crawler = crawler;
 	}
 
+	/**
+	 * <p>
+	 * Initialises the data-access objects used to write the scraped
+	 * data.
+	 * </p>
+	 * 
+	 * @param storageRoot - the root folder of the database.
+	 */
 	private void initDAOs(File storageRoot) {
 		cifDao = new CifFileDAO(storageRoot);
 		articleMetadataDao = new ArticleMetadataDAO(storageRoot);
@@ -43,6 +60,13 @@ public class CrawlerTask {
 		pKeyVsDoiIndex = new PrimaryKeyVsDoiIndex(storageRoot);
 	}
 
+	/**
+	 * <p>
+	 * Starts off the crawler, which finds CIFs and then writes the
+	 * scraped data out to the database.
+	 * </p> 
+	 * 
+	 */
 	public void crawl() {
 		for (ArticleDetails ad : crawler.getDetailsForCurrentArticles()) {
 			for (SupplementaryFileDetails sfd : ad.getSuppFiles()) {
@@ -63,6 +87,15 @@ public class CrawlerTask {
 		}
 	}
 	
+	/**
+	 * <p>
+	 * Writes a file out containing the provided article metadata.
+	 * </p>
+	 * 
+	 * @param primaryKey - the key for which you are inserting the
+	 * metadata.
+	 * @param sfd - details about the supplementary file.
+	 */
 	private void insertArticleMetadata(int primaryKey, SupplementaryFileDetails sfd) {
 		boolean success = articleMetadataDao.insert(primaryKey, sfd.toString());
 		if (!success) {
@@ -70,6 +103,15 @@ public class CrawlerTask {
 		}
 	}
 	
+	/**
+	 * <p>
+	 * Updates the index of the CIFs origin DOI against the CIFs 
+	 * file name at the site it was scraped from. 
+	 * </p>
+	 * 
+	 * @param doi of the article the CIF was obtained from.
+	 * @param filename of the CIF at the site it was scraped from.
+	 */
 	private void updateDoiVsCifFilenameIndex(DOI doi, String filename) {
 		boolean success = doiVsCifFilenameIndex.insert(doi, filename);
 		if (!success) {
@@ -77,6 +119,15 @@ public class CrawlerTask {
 		}
 	}
 
+	/**
+	 * <p>
+	 * Updates an index of the primary key for the CIF against
+	 * the DOI of the article the CIF was scraped from.
+	 * </p>
+	 * 
+	 * @param primaryKey of the CIF in the database.
+	 * @param doi of the article the CIF was obtained from.
+	 */
 	private void updatePKeyVsDoiIndex(int primaryKey, DOI doi) {
 		boolean success = pKeyVsDoiIndex.insert(primaryKey, doi);
 		if (!success) {
@@ -84,11 +135,33 @@ public class CrawlerTask {
 		}
 	}
 
+	/**
+	 * <p>
+	 * Writes the CIF described by the provided 
+	 * <code>SupplementaryFileDetails</code> to the database.
+	 * </p>
+	 * 
+	 * @param sfd - details of the CIF to be written.
+	 * 
+	 * @return the database primary key that the CIF was 
+	 * assigned when it was inserted into the database.
+	 */
 	private int writeCIF(SupplementaryFileDetails sfd) {
 		String cifContents = new CrawlerHttpClient().getResourceString(sfd.getURI());
 		return cifDao.insertCif(cifContents);
 	}
 
+	/**
+	 * <p>
+	 * Checks whether the provided SupplementaryFileDetails
+	 * is an instance of CifFileDetails.
+	 * </p>
+	 * 
+	 * @param sfd - details to be checked.
+	 * 
+	 * @return true if the instance is a CifFileDetails, false
+	 * if not.
+	 */
 	private boolean isCifFile(SupplementaryFileDetails sfd) {
 		if (sfd instanceof CifFileDetails) {
 			return true;
@@ -97,6 +170,13 @@ public class CrawlerTask {
 		}
 	}
 
+	/**
+	 * <p>
+	 * Main method is only for demonstration of class use.  Does not
+	 * require any arguments.
+	 * </p>
+	 * 
+	 */
 	public static void main(String[] args) throws IOException {
 		String root = "c:/work/crystaleye1.2-data";
 		File storageRoot = new File(root);
