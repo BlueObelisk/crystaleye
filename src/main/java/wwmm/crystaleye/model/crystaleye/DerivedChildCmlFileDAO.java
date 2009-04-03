2070e58fae5c519851364d2f763f4a3a682cebcf
ned24 <ned24@cam.ac.uk>
2009-04-03 23:12:34 +0000

    Added Cml2DerivedCmlTask.

--- src/main/java/wwmm/crystaleye/model/crystaleye/DerivedChildCmlFileDAO.java
+++ src/main/java/wwmm/crystaleye/model/crystaleye/DerivedChildCmlFileDAO.java
@@ -4,17 +4,26 @@ import java.io.File;
 
 import wwmm.crystaleye.model.core.ChildSecondaryFileDAO;
 
-public class ChildCmlDAO extends ChildSecondaryFileDAO {
+/**
+ * <p>
+ * Data-access class for accessing the 'derived' CML files in 
+ * CrystalEye that are literal translations of the CIFs.
+ * </p>
+ * 
+ * @author Nick Day
+ * @version 0.1
+ */
+public class DerivedChildCmlFileDAO extends ChildSecondaryFileDAO {
 	
 	/**
 	 * An implementing subclass of ChildSecondaryFileDAO needs to set the
 	 * file extension to be used by the primary file of the database.
 	 */
 	static {
-		fileExtension = ".cml";
+		fileExtension = ".derived.cml";
 	}
 
-	public ChildCmlDAO(File storageRoot) {
+	public DerivedChildCmlFileDAO(File storageRoot) {
 		super(storageRoot);
 	}
 	
