a5f4ab5d9d054dfec9bd3ff865a3f5905d306148
ned24 <ned24@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2009-08-25 09:01:09 +0000

    First commit of branch 1.0.y

---
+++ src/main/java/wwmm/crystaleye/fetch/RscBacklog.java
@@ -0,0 +1,149 @@
+package wwmm.crystaleye.fetch;
+
+import static wwmm.crystaleye.CrystalEyeConstants.X_XHTML;
+
+import java.io.File;
+
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Nodes;
+
+import org.apache.log4j.Logger;
+
+import wwmm.crystaleye.IOUtils;
+
+
+public class RscBacklog extends Fetcher {
+	
+	private static final Logger LOG = Logger.getLogger(RscBacklog.class);
+
+	private static final String HOMEPAGE_PREFIX = "http://www.rsc.org";
+	private static final String PUBLISHER_ABBREVIATION = "rsc";
+
+	String journalAbbreviation;
+	String year;
+	String issue;
+	String volume = "0";
+
+	public RscBacklog(String propertiesFile, String journalAbbreviation, String year, String issue) {
+		super(PUBLISHER_ABBREVIATION, propertiesFile);
+		setYear(year);
+		setJournalAbbreviation(journalAbbreviation);
+		setIssue(issue);
+	}
+
+	public void setJournalAbbreviation(String journalAbbreviation) {
+		this.journalAbbreviation = journalAbbreviation;
+		if ("ob".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-2003)+1);
+		} else if ("ce".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-1999)+1);
+		} else if ("nj".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-1998)+22);
+		} else if ("jm".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-1991)+1);
+		} else if ("cp".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-1999)+1);
+		} else if ("fd".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-1991)+92);
+		} else if ("gc".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-1999)+1);
+		} else if ("em".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-1999)+1);
+		} else if ("np".equalsIgnoreCase(journalAbbreviation)) {
+			volume = String.valueOf((Integer.parseInt(year)-1984)+1);
+		}
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public void setIssue(String issue) {
+		this.issue = issue;
+	}
+
+	public void fetch() {
+		String writeDir = properties.getWriteDir();
+		String url = "http://rsc.org/Publishing/Journals/"+journalAbbreviation.toLowerCase()+"/article.asp?Journal="+journalAbbreviation+"81&VolumeYear="+year+volume+"&Volume="+volume+"&JournalCode="+journalAbbreviation+"&MasterJournalCode="+journalAbbreviation+"&SubYear="+year+"&type=Issue&Issue="+issue+"&x=11&y=5";
+		Document doc = IOUtils.parseWebPageMinusComments(url);
+		Nodes articleLinks = doc.query("//x:a[contains(@href,'/Publishing/Journals/"+journalAbbreviation.toUpperCase()+"/article.asp?doi=') and preceding-sibling::x:strong[contains(text(),'DOI:')]]", X_XHTML);
+		if (articleLinks.size() > 0) {
+			LOG.info("Reading "+journalAbbreviation.toUpperCase()+" issue "+issue+" at "+url);
+		}
+		for (int i = 0; i < articleLinks.size(); i++) {
+			String articleUrl = HOMEPAGE_PREFIX+((Element)articleLinks.get(i)).getAttributeValue("href");
+			Document articleDoc = IOUtils.parseWebPageMinusComments(articleUrl);
+			Nodes suppdataLinks = articleDoc.query("//x:a[contains(text(),'Electronic supplementary information')]", X_XHTML);
+			for (int j = 0; j < suppdataLinks.size(); j++) {
+				sleep();
+				String suppdataUrl = HOMEPAGE_PREFIX+((Element)suppdataLinks.get(j)).getAttributeValue("href");
+				Document suppdataDoc = IOUtils.parseWebPageMinusComments(suppdataUrl);
+				Nodes cifLinks = suppdataDoc.query("//x:a[text()='Crystal structure data'] | //x:a[text()='Crystal Structure Data'] | //x:a[text()='Crystal Structure data'] | //x:a[text()='Crystal data'] | //x:a[text()='Crystal Data'] | //x:a[text()='Crystallographic Data'] | //x:a[text()='Crystallographic data']", X_XHTML);
+				int cifLinkNum = 0;
+				for (int k = 0; k < cifLinks.size(); k++) {
+					cifLinkNum++;
+					sleep();
+					String cifFileName = ((Element)cifLinks.get(k)).getAttributeValue("href");
+					int idx = suppdataUrl.lastIndexOf("/");
+					String parent = suppdataUrl.substring(0, idx);
+					int idx1 = cifFileName.indexOf(".");
+					String cifId = cifFileName.substring(0, idx1);
+					String cifLink = parent+"/"+cifFileName;
+
+					String cif = IOUtils.fetchWebPage(cifLink);
+					String path = writeDir+File.separator+PUBLISHER_ABBREVIATION+File.separator+journalAbbreviation+File.separator+year+File.separator+issue+File.separator+cifId+File.separator+cifId+"sup"+cifLinkNum+".cif";
+					IOUtils.writeText(cif, path);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String props = "E:\\crystaleye-new\\docs\\cif-flow-props.txt";
+		RscBacklog ore = new RscBacklog(props, "", "", "");
+		
+		ore = new RscBacklog(props, "ob", "2008", "22");
+		ore.fetch();
+		
+		for (int i = 46; i <49; i++) {
+			ore = new RscBacklog(props, "dt", "2008", String.valueOf(i));
+			ore.fetch();
+		}
+		for (int i = 1; i <11; i++) {
+			ore = new RscBacklog(props, "dt", "2009", String.valueOf(i));
+			ore.fetch();
+		}
+		
+		ore = new RscBacklog(props, "gc", "2008", "12");
+		ore.fetch();
+		ore = new RscBacklog(props, "gc", "2009", "1");
+		ore.fetch();
+		ore = new RscBacklog(props, "gc", "2009", "2");
+		ore.fetch();
+		
+		for (int i = 46; i <49; i++) {
+			ore = new RscBacklog(props, "jm", "2008", String.valueOf(i));
+			ore.fetch();
+		}
+		for (int i = 1; i <11; i++) {
+			ore = new RscBacklog(props, "jm", "2009", String.valueOf(i));
+			ore.fetch();
+		}
+
+		ore = new RscBacklog(props, "nj", "2008", "12");
+		ore.fetch();
+		ore = new RscBacklog(props, "nj", "2009", "1");
+		ore.fetch();
+		ore = new RscBacklog(props, "nj", "2009", "2");
+		ore.fetch();
+		
+		
+		ore = new RscBacklog(props, "ob", "2008", "24");
+		ore.fetch();
+		for (int i = 1; i <6; i++) {
+			ore = new RscBacklog(props, "ob", "2009", String.valueOf(i));
+			ore.fetch();
+		}
+	}
+}
---
+++ src/main/java/wwmm/crystaleye/templates/feeds/Rss1.java
@@ -0,0 +1,57 @@
+package wwmm.crystaleye.templates.feeds;
+
+import static wwmm.crystaleye.CrystalEyeConstants.CRYSTALEYE_DATE_FORMAT;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.ParsingException;
+import nu.xom.ValidityException;
+
+public class Rss1 {
+	
+	String title;
+	String subTitle;
+	String author;
+	String link;
+
+	public Rss1(String title, String subTitle, String author, String link) {
+		super();
+		this.title = title;
+		this.subTitle = subTitle;
+		this.author = author;
+		this.link = link;
+	}
+
+	public Document getFeed() throws ValidityException, ParsingException, IOException {
+		Date date = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat(CRYSTALEYE_DATE_FORMAT);
+		String dNow = formatter.format(date);
+		
+		String feed = "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" " +
+		"xmlns=\"http://purl.org/rss/1.0/\" " +
+		"xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" " +
+		"xmlns:taxo=\"http://purl.org/rss/1.0/modules/taxonomy/\" " +
+		"xmlns:sy=\"http://purl.org/rss/1.0/modules/syndication/\" " +
+		"xmlns:dc=\"http://purl.org/dc/elements/1.1/\" "+
+		"xmlns:cml=\"http://www.xml-cml.org/schema\"> " +
+		  "<channel rdf:about=\""+link+"\">"+
+		    "<title>"+title+"</title>"+
+		    "<link>"+link+"</link>"+
+		    "<description>"+subTitle+"</description>"+
+		    "<items>"+
+		      "<rdf:Seq>"+
+		      "</rdf:Seq>"+
+		    "</items>"+
+		    "<dc:creator>"+author+"</dc:creator>"+
+		    "<dc:date>"+dNow+"</dc:date>"+
+		  "</channel>"+
+		"</rdf:RDF>";
+		return new Builder().build(new BufferedReader(new StringReader(feed)));
+	}
+}
